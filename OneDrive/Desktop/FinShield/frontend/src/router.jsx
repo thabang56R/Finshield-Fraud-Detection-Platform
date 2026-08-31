92c7a39470f9be5c1f0dd931248366d237b84d73
thabang56R <thabang56@hotmail.com>
2026-08-31 14:21:51 +0200

    Added front end files

---
+++ OneDrive/Desktop/FinShield/frontend/src/router.jsx
@@ -0,0 +1,6 @@
+import { createRouter } from "@tanstack/react-router";
+import { routeTree } from "./file-routes"; // auto-generated file you showed
+
+export const router = createRouter({
+  routeTree,
+});
---
+++ OneDrive/Desktop/FinShield/frontend/src/routes/__root.jsx
@@ -0,0 +1,5 @@
+import { RootRoute } from "@tanstack/react-router";
+
+export const Route = new RootRoute({
+  component: () => <div className="p-6"><h1>FinShield Delight</h1></div>,
+});
