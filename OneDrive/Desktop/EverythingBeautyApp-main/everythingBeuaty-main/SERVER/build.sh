0fe351192457f06e62f252b125d26fdd192f8e9e
Thabang Rakeng <thabang56@hotmail.com>
2026-09-01 14:29:58 +0200

    Delete OneDrive/Desktop/EverythingBeautyApp-main directory

--- OneDrive/Desktop/EverythingBeautyApp-main/everythingBeuaty-main/SERVER/build.sh
+++
@@ -1,5 +0,0 @@
-set -o errexit
-
-pip install -r SERVER/newproject/requirements.txt
-python SERVER/newproject/manage.py collectstatic --noinput 
-python SERVER/newproject/manage.py migrate
--- OneDrive/Desktop/EverythingBeautyApp-main/everythingBeuaty-main/SERVER/newproject/check_columns.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("db.sqlite3")
-cursor = conn.cursor()
-cursor.execute("PRAGMA table_info(api_booking);")
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
-conn.close()
